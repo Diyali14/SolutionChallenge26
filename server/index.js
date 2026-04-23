194721c70b9913772738ecacdd9a607c4fe4e74e
Sibangi15 <sibangichakraborty2005@gmail.com>
2026-04-23 13:05:44 +0530

    fix port and routes

--- server/index.js
+++ server/index.js
@@ -153,7 +153,7 @@ app.post("/run-matching", async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
